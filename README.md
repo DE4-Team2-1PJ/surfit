1af36b42ca4116e42cf3c676b50cb488c8a0c870
Kim Taehyun <taewrui07@konkuk.ac.kr>
2024-10-16 00:17:48 +0900

    추가된 컬럼 수집, 카테고리 분류, 크롤링 결과 추가

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
 # surfit
 Surfit의 채용공고 크롤링
-- "데이터 엔지니어" 카테고리에 해당하는 URL에서 공고 제목 수집
-- 공고 제목으로 링크 들어가서 회사 이름, 공고 주소, 마감일 수집
+- 각 카테고리에 해당하는 URL에서 공고 제목 수집
+    - **데이터 엔지니어, 데이터 사이언티스트, 머신러닝 엔지니어, 비즈니스 분석가** 총 4개
+    - 추후 카테고리 통일해서 재분류 예정
+- 공고 제목으로 링크 들어가서 회사 이름, 공고 주소, 마감일, 스택, 지역, 신입/경력 수집
+    - 지역의 경우 **"100% 원격근무"**가 존재
+
+### 크롤링 결과
+{'title': 'AI Core Researcher (경력)', 'company_name': '달파(Dalpha)', 'detail_url': 'https://jobs.surfit.io/post/68705', 'end_date': '채용시까지', 'platform_name': 'Surfit', 'category_name': '데이터 엔지니어', 'stack': ['Python', 'GitHub', 'Docker', 'Linux', 'Pytorch', 'Terraform', 'AWS', 'K8S'], 'region': '서울특별시 관 악구', 'career': '3년 이상'}
+{'title': 'AI Engineer & Data Scientist', 'company_name': 'Nimbyx(Evident)', 'detail_url': 'https://jobs.surfit.io/post/80897', 'end_date': '채용시까지', 'platform_name': 'Surfit', 'category_name': '데이터 엔지니어', 'stack': None, 'region': '100% 원격근무', 'career': '경력직'}
+{'title': 'AI Engineer & Data Scientist', 'company_name': 'Nimbyx(Evident)', 'detail_url': 'https://jobs.surfit.io/post/80897', 'end_date': '채용시까지', 'platform_name': 'Surfit', 'category_name': '데이터 사이언티스트', 'stack': None, 'region': '100% 원격근무', 'career': '경력직'}
+{'title': 'AI Core Researcher (경력)', 'company_name': '달파(Dalpha)', 'detail_url': 'https://jobs.surfit.io/post/68705', 'end_date': '채용시까지', 'platform_name': 'Surfit', 'category_name': '머신러닝 엔지니어', 'stack': ['Python', 'GitHub', 'Docker', 'Linux', 'Pytorch', 'Terraform', 'AWS', 'K8S'], 'region': '서울특별시  관악구', 'career': '3년 이상'}
+{'title': 'AI Engineer & Data Scientist', 'company_name': 'Nimbyx(Evident)', 'detail_url': 'https://jobs.surfit.io/post/80897', 'end_date': '채용시까지', 'platform_name': 'Surfit', 'category_name': '머신러닝 엔지니어', 'stack': None, 'region': '100% 원격근무', 'career': '경력직'}
+{'title': '의료영상 AI Engineer(Junior)', 'company_name': '웨이센', 'detail_url': 'https://jobs.surfit.io/post/111135', 'end_date': '채용시까지', 'platform_name': 'Surfit', 'category_name': '머신러닝 엔지니어', 'stack': ['C', 'Python', 'Java', 'Scipy', 'Pandas', 'NumPy', 'TensorFlow', 'Pytorch'], 'region': '서울특별시 강남구', 'career': '경력 무관'}
+{'title': '의료영상 AI Engineer(Senior)', 'company_name': '웨이센', 'detail_url': 'https://jobs.surfit.io/post/111260', 'end_date': '채용시까지', 'platform_name': 'Surfit', 'category_name': '머신러닝 엔지니어', 'stack': ['Python', 'TensorFlow', 'Pytorch', 'Caffe'], 'region': '서울특별시 강남구', 'career': '5년 이상'}      
+{'title': '[flex] HR Partner', 'company_name': '플렉스(flex)', 'detail_url': 'https://jobs.surfit.io/post/68205', 'end_date': '채용시까지', 'platform_name': 'Surfit', 'category_name': '비즈니스 분석가', 'stack': ['HR', 'HRM', 'HRD', 'HRIS', '인사', '인사컨설팅', 'HR 운영'], 'region': '경기도 성남시', 'career': '3년 이상'}
--- surfit.py
+++ surfit.py
@@ -3,32 +3,56 @@ from selenium import webdriver
 import time
 
 jobs = []
-base_url = "https://jobs.surfit.io/develop/data" # 데이터 엔지니어 카테고리만 수집
-
+# URL에 따른 카테고리 이름
+category_to_name = {
+    "develop/data": "데이터 엔지니어",
+    "develop/data-science": "데이터 사이언티스트",
+    "develop/bigdata-ai-ml": "머신러닝 엔지니어",
+    "planning/biz-analyst": "비즈니스 분석가"
+}
+cat_key = category_to_name.keys()
 driver = webdriver.Chrome(executable_path=r"C:\Users\User\Desktop\chromedriver-win64\chromedriver.exe")
-driver.get(base_url)
-time.sleep(1)
 
-driver.execute_script("window.scrollTo(0, document.body.scrollHeight);") # 스크롤하는 동작이 없으면 공고가 로드되지 않음
-time.sleep(5)
-soup = BeautifulSoup(driver.page_source, 'html.parser')
+for cat in cat_key:
+    base_url = f"https://jobs.surfit.io/{cat}" 
 
-job_titles = soup.find_all('span', class_="post-title")
-print(job_titles)
-for job in job_titles:
-    job_info = {'title': job.get_text().strip()} # 공고 제목
-    job_url = job.find_parent('a')['href']
-    
-    driver.get(job_url)
-    time.sleep(3)
+    driver.get(base_url)
+    time.sleep(1)
+
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);") # 스크롤하는 동작이 없으면 공고가 로드되지 않음
+    time.sleep(5)
     soup = BeautifulSoup(driver.page_source, 'html.parser')
-    
-    # 공고제목 - title, 회사이름 - company_name, 공고주소 - detail_url, 이미지주소 - img_link, 등록일 - pub_date, 마감일 - end_date
-    job_info['company_name'] = soup.select_one('#app > div.jobs > div > div > div > div > div.iTVFTT > div.zpulQ > div > div > span').get_text().strip()
-    job_info['detail_url'] = job_url
-    job_info['end_date'] = soup.select_one("#app > div.jobs > div > div > div > div > div.content-area.isNepE > div > div > ul > li:nth-child(3) > span.value").get_text().strip()
-    job_info['platform_name'] = "Surfit"
-    jobs.append(job_info)
+
+    job_titles = soup.find_all('span', class_="post-title")
+    #print(job_titles)
+    for job in job_titles:
+        job_info = {'title': job.get_text().strip()} # 공고 제목
+        job_url = job.find_parent('a')['href']
+        
+        driver.get(job_url)
+        time.sleep(3)
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
+        
+        # 공고제목 - title, 회사이름 - company_name, 공고주소 - detail_url, 이미지주소 - img_link, 
+        # 등록일 - pub_date, 마감일 - end_date, 카테고리 - category_name, 기술스택 - stack
+        # 지역 - region, 신입/경력 - career
+        job_info['company_name'] = soup.select_one('#app > div.jobs > div > div > div > div > div.iTVFTT > div.zpulQ > div > div > span').get_text().strip()
+        job_info['detail_url'] = job_url
+        job_info['end_date'] = soup.select_one("#app > div.jobs > div > div > div > div > div.content-area.isNepE > div > div > ul > li:nth-child(3) > span.value").get_text().strip()
+        job_info['platform_name'] = "Surfit"
+        job_info['category_name'] = category_to_name[cat]
+        
+        stacks = soup.select_one("#app > div.jobs > div > div > div > div > div.content-area.isNepE > div > div > div.job-post-info > div:nth-child(2) > ul")
+        if not stacks:
+            job_info['stack'] = None
+        else:
+            stacks = stacks.find_all('li')
+            job_info['stack'] = [stack.get_text().strip() for stack in stacks]
+            
+        region = soup.find("span", class_="location-text").get_text().strip()
+        job_info['region'] = " ".join(region.split()[:2])  # 주소 텍스트에서 서울특별시 **구, 경기도 **시 까지만 저장
+        job_info['career'] = soup.select_one("#app > div.jobs > div > div > div > div > div.content-area.isNepE > div > div > ul > li:nth-child(1) > span.value").get_text().strip()
+        jobs.append(job_info)
 
 driver.quit()
 
